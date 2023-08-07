b0f1da404b147937c0ac4b61c2d7e5de355d12f9
Hen Tzarfati <henz123@gmail.com>
2023-08-08 00:11:30 +0300

    added impersonation to Approval Flow Table lines based on some span with "data-email" attribute on it.

--- background.js
+++ background.js
@@ -13,6 +13,16 @@ chrome.runtime.onMessage.addListener(async function (
 });
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (activeInfo.tabId === tabId && changeInfo.url) {
+      chrome.tabs.sendMessage(
+          activeInfo.tabId,
+          { action: "onTabUrlChange" },
+          () => chrome.runtime.lastError
+      );
+    }
+  })
+
   chrome.tabs.sendMessage(
     activeInfo.tabId,
     { action: "onTabChange" },
--- content.js
+++ content.js
@@ -1,16 +1,55 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message?.action === "onTabChange") {
-    handleCurrentImpersonation();
-    return;
-  }
+const SCAN_INTERVAL_MS = 1000; // TODO: MAKE THIS CHANGEABLE THROUGH SOME HIDDEN SETTINGS PANEL
+const TEST_MODE = true;  // TODO: MAKE THIS CHANGEABLE THROUGH SOME HIDDEN SETTINGS PANEL
+let intervalId = null;
 
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  switch (message?.action) {
+    case "onTabChange":
+      handleCurrentImpersonation();
+      return;
+    case "onTabUrlChange":
+      handleApprovalFlowTableImpersonation();
+      return;
+  }
   if (message?.data?.email) {
-    localStorage.setItem("forceCustomer", message.data.email);
-    location.reload();
+    impersonate(message.data.email);
   }
 
   sendResponse();
 });
+
+function impersonate(email) {
+  localStorage.setItem("forceCustomer", email);
+  location.reload();
+}
+
+function handleApprovalFlowTableImpersonation() {
+  if (intervalId)
+    clearInterval(intervalId);
+
+  intervalId = setInterval(() => {
+    const approvalFlowTableLines = document.querySelector('.approval-flow-table');
+    if (approvalFlowTableLines) {
+
+      if (!TEST_MODE) clearInterval(intervalId); // We found the approval flow table. stop searching for it. in test-mode it lets you play with the dom and see the effects (for example, set data-email to some approvalName and see how it changes on-the-fly)
+
+      approvalFlowTableLines.childNodes.forEach(aftl => {
+        const approvalName = aftl.querySelector("span[data-email]:first-child");
+        if (!approvalName) return;
+
+        const emailFromName = approvalName.dataset.email; // FYI, you could also use: aftl.getAttribute('data-email')
+        if (!emailFromName) return;
+
+        approvalName.removeAttribute("data-email"); //remove data-email! that will force emailFromName to be undefined after handling. that means: 1. if we don't have data-email, we don't care. and if we had it, but handled it, we don't care about it either (two birds at one shot).
+        approvalName.style.cursor = 'pointer';
+        approvalName.addEventListener('click', (e) => {
+          e.stopPropagation();
+          impersonate(emailFromName);
+        }, {once: true}); // Once triggered, removing the listener.
+      });
+    }
+  }, SCAN_INTERVAL_MS)
+}
 
 // Send the current impersonation to background
 function handleCurrentImpersonation() {
@@ -46,5 +85,6 @@ function parseHostnameToEnv(_hostname) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  handleApprovalFlowTableImpersonation();
   handleCurrentImpersonation();
 });
