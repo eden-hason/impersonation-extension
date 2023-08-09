218f8be9ac9a927e6f81bc20dd642617455b1f20
eden-hason <eden@hason.me>
2023-08-09 15:09:15 +0300

    feat: add version element and an about alert

--- popup.js
+++ popup.js
@@ -1,18 +1,4 @@
-const emailsListElement = document.getElementById("emails-list");
-
-emailsListElement.addEventListener("click", function (e) {
-  const selectedEmail = e.target.innerText;
-
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(
-      activeTab.id,
-      { data: { email: selectedEmail } },
-      () => window.close()
-    );
-  });
-});
-
+// Handle render of the list
 async function renderList() {
   const emailsResult = await chrome.storage.local.get("emails");
   const envResult = await chrome.storage.local.get("env");
@@ -38,6 +24,30 @@ function getButtonElement(email) {
   return button;
 }
 
+// Handle list item click
+const emailsListElement = document.getElementById("emails-list");
+
+emailsListElement.addEventListener("click", function (e) {
+  const selectedEmail = e.target.innerText;
+
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(
+      activeTab.id,
+      { data: { email: selectedEmail } },
+      () => window.close()
+    );
+  });
+});
+
+// Handle version container click
+const versionButtonElement = document.getElementById("version-button");
+
+versionButtonElement.addEventListener("click", function () {
+  alert("Version 1.0 \n Eden Hason & Hen Tzarfati");
+});
+
+// Handle page load
 document.addEventListener("DOMContentLoaded", function () {
   renderList();
 });
