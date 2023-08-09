3cb44491e36bdac5f940e8867b79195ef18dd2e9
eden-hason <eden@hason.me>
2023-08-09 14:24:23 +0300

    refactor: extract function from background script to content script

--- background.js
+++ background.js
@@ -1,27 +1,13 @@
-const MAX_EMAILS_TO_STORE = 5;
-
-chrome.runtime.onMessage.addListener(async function (
-  message,
-  sender,
-  sendResponse
-) {
-  if (message?.data) {
-    await setEmailToLocalStorage(message.data.email, message.data.env);
-  }
-
-  sendResponse();
-});
-
 chrome.tabs.onActivated.addListener(function (activeInfo) {
   chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (activeInfo.tabId === tabId && changeInfo.url) {
       chrome.tabs.sendMessage(
-          activeInfo.tabId,
-          { action: "onTabUrlChange" },
-          () => chrome.runtime.lastError
+        activeInfo.tabId,
+        { action: "onTabUrlChange" },
+        () => chrome.runtime.lastError
       );
     }
-  })
+  });
 
   chrome.tabs.sendMessage(
     activeInfo.tabId,
@@ -29,20 +15,3 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
     () => chrome.runtime.lastError
   );
 });
-
-async function setEmailToLocalStorage(email, env) {
-  const storeResult = await chrome.storage.local.get("emails");
-  const storedEmails = storeResult.emails || {};
-
-  if (!storedEmails[env]) {
-    storedEmails[env] = [email];
-  } else {
-    const emailsSet = new Set([email, ...storedEmails[env]]);
-    storedEmails[env] = Array.from(emailsSet).splice(0, MAX_EMAILS_TO_STORE);
-  }
-
-  chrome.storage.local.set({
-    env,
-    emails: storedEmails,
-  });
-}
--- content.js
+++ content.js
@@ -1,15 +1,14 @@
 const SCAN_INTERVAL_MS = 1000; // TODO: MAKE THIS CHANGEABLE THROUGH SOME HIDDEN SETTINGS PANEL
-const TEST_MODE = true;  // TODO: MAKE THIS CHANGEABLE THROUGH SOME HIDDEN SETTINGS PANEL
+const TEST_MODE = true; // TODO: MAKE THIS CHANGEABLE THROUGH SOME HIDDEN SETTINGS PANEL
+const MAX_EMAILS_TO_STORE = 5;
 let intervalId = null;
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   switch (message?.action) {
     case "onTabChange":
       handleCurrentImpersonation();
-      return;
     case "onTabUrlChange":
       handleApprovalFlowTableImpersonation();
-      return;
   }
   if (message?.data?.email) {
     impersonate(message.data.email);
@@ -24,44 +23,42 @@ function impersonate(email) {
 }
 
 function handleApprovalFlowTableImpersonation() {
-  if (intervalId)
-    clearInterval(intervalId);
+  if (intervalId) clearInterval(intervalId);
 
   intervalId = setInterval(() => {
-    const approvalFlowTableLines = document.querySelector('.approval-flow-table');
+    const approvalFlowTableLines = document.querySelector(
+      ".approval-flow-table"
+    );
     if (approvalFlowTableLines) {
-
       if (!TEST_MODE) clearInterval(intervalId); // We found the approval flow table. stop searching for it. in test-mode it lets you play with the dom and see the effects (for example, set data-email to some approvalName and see how it changes on-the-fly)
 
-      approvalFlowTableLines.childNodes.forEach(line => {
+      approvalFlowTableLines.childNodes.forEach((line) => {
         const elementWithDataEmail = line.querySelector("[data-email]");
         if (!elementWithDataEmail) return;
 
         const email = elementWithDataEmail.dataset.email; // FYI, you could also use: line.getAttribute('data-email')
         elementWithDataEmail.removeAttribute("data-email"); //remove data-email attribute. that will force elementWithDataEmail to be undefined in the next intervals. It means that we won't handle it again and waste resources (for example: register a lot of click events).
-        elementWithDataEmail.style.cursor = 'pointer';
-        elementWithDataEmail.addEventListener('click', (e) => {
-          e.stopPropagation();
-          impersonate(email);
-        }, {once: true}); // Once triggered, removing the listener.
+        elementWithDataEmail.style.cursor = "pointer";
+        elementWithDataEmail.addEventListener(
+          "click",
+          (e) => {
+            e.stopPropagation();
+            impersonate(email);
+          },
+          { once: true }
+        ); // Once triggered, removing the listener.
       });
     }
-  }, SCAN_INTERVAL_MS)
+  }, SCAN_INTERVAL_MS);
 }
 
-// Send the current impersonation to background
 function handleCurrentImpersonation() {
   const email = localStorage.getItem("forceCustomer");
   const env = parseHostnameToEnv();
 
   if (!email || !env) return;
 
-  chrome.runtime.sendMessage({
-    data: {
-      env,
-      email: localStorage.getItem("forceCustomer"),
-    },
-  });
+  setEmailToLocalStorage(email, env);
 }
 
 function parseHostnameToEnv(_hostname) {
@@ -80,6 +77,23 @@ function parseHostnameToEnv(_hostname) {
   }
 
   return env;
+}
+
+async function setEmailToLocalStorage(email, env) {
+  const storeResult = await chrome.storage.local.get("emails");
+  const storedEmails = storeResult.emails || {};
+
+  if (!storedEmails[env]) {
+    storedEmails[env] = [email];
+  } else {
+    const emailsSet = new Set([email, ...storedEmails[env]]);
+    storedEmails[env] = Array.from(emailsSet).splice(0, MAX_EMAILS_TO_STORE);
+  }
+
+  chrome.storage.local.set({
+    env,
+    emails: storedEmails,
+  });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
